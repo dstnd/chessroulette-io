f1a406a4160ae737eeb9107fb42915e5126729da
Gabriel C. Troia <catalin.troia@gmail.com>
2020-05-04 01:53:39 +0300

    Make it ready to be a lib

---
+++ src/records/index.ts
@@ -0,0 +1,9 @@
+export * from './HttpPayload';
+export * from './PeerMessagingPayload';
+export * from './SignalingPayload';
+
+// export default {
+//   HttpPayload,
+//   PeerMessagingPayload,
+//   SignalingPayload,
+// };
